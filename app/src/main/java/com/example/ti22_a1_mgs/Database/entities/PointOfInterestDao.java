8a159db54e24c454cc109aed31a3a1080460faef
Lucas Prins <lljPrins@avans.nl>
2019-12-12 17:14:20 +0100

    Added The Database

---
+++ app/src/main/java/com/example/ti22_a1_mgs/Database/entities/PointOfInterestDao.java
@@ -0,0 +1,30 @@
+package com.example.ti22_a1_mgs.Database.entities;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface PointOfInterestDao {
+
+    @Insert
+    void insert(PointOfInterest pointOfInterest);
+
+    @Update
+    void update(PointOfInterest pointOfInterest);
+
+    @Delete
+    void delete(PointOfInterest pointOfInterest);
+
+
+    @Query("DELETE FROM point_of_interest_table")
+    void deleteAllPointsOfInterest();
+
+    @Query("SELECT * FROM point_of_interest_table ORDER BY location DESC")
+    LiveData<List<PointOfInterest>> getAllPointsOfInterest();
+}
---
+++ app/src/main/java/com/example/ti22_a1_mgs/Database/entities/WaypointDao.java
@@ -0,0 +1,31 @@
+package com.example.ti22_a1_mgs.Database.entities;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface WaypointDao {
+
+    @Insert
+    void insert(Waypoint waypoint);
+
+    @Update
+    void update(Waypoint waypoint);
+
+    @Delete
+    void delete(Waypoint waypoint);
+
+
+    @Query("DELETE FROM waypoint_table")
+    void deleteAllWaypoints();
+
+    @Query("SELECT * FROM waypoint_table ORDER BY number DESC")
+    LiveData<List<Waypoint>> getAllWaypoints();
+
+}
